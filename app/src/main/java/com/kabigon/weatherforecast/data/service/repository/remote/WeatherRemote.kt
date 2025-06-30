b5d50864ef99aab52ef2b67e4fecb17190eb8b93
Jakrapun <jakrapun@ava.fund>
2025-07-01 06:45:55 +0700

    feat: add weather api remote

---
+++ app/src/main/java/com/kabigon/weatherforecast/data/service/repository/remote/WeatherRemote.kt
@@ -0,0 +1,42 @@
+package com.kabigon.weatherforecast.data.service.repository.remote
+
+import com.kabigon.weatherforecast.data.base.ApiManager
+import com.kabigon.weatherforecast.data.model.response.WeatherResponse
+import com.kabigon.weatherforecast.data.service.api.WeatherService
+import retrofit2.Response
+
+interface IWeatherRemote {
+    suspend fun getWeather(
+        version: String,
+        cityName: String,
+        apiKey: String,
+        units: String
+    ):Response<WeatherResponse>
+}
+
+class WeatherRemote(
+    private val apiManager: ApiManager
+): IWeatherRemote {
+
+    companion object {
+        private const val API_VERSION = "2.5"
+    }
+
+    private val weatherService = apiManager.init(
+        "http://api.openweathermap.org/"
+    ).create(WeatherService::class.java)
+
+    override suspend fun getWeather(
+        version: String,
+        cityName: String,
+        apiKey: String,
+        units: String
+    ): Response<WeatherResponse> {
+        return weatherService.getWeather(
+            API_VERSION,
+            cityName,
+            apiKey,
+            units
+        )
+    }
+}
--- app/src/main/java/com/kabigon/weatherforecast/di/NetworkModule.kt
+++ app/src/main/java/com/kabigon/weatherforecast/di/NetworkModule.kt
@@ -2,6 +2,8 @@ package com.kabigon.weatherforecast.di
 
 import com.kabigon.weatherforecast.data.base.ApiManager
 import com.kabigon.weatherforecast.data.base.NetworkClient
+import com.kabigon.weatherforecast.data.service.repository.remote.IWeatherRemote
+import com.kabigon.weatherforecast.data.service.repository.remote.WeatherRemote
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -17,6 +19,12 @@ val networkModule = module {
     factory {
         NetworkClient(
             androidContext()
+        )
+    }
+
+    factory<IWeatherRemote> {
+        WeatherRemote(
+            get()
         )
     }
 }

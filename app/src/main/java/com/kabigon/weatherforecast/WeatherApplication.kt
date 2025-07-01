7a99870db36acece133ce33ad15f72a2a35aef32
Jakrapun <jakrapun@ava.fund>
2025-07-01 16:28:42 +0700

    feat: add weather application that inject koin modules

---
+++ app/src/main/java/com/kabigon/weatherforecast/WeatherApplication.kt
@@ -0,0 +1,25 @@
+package com.kabigon.weatherforecast
+
+import android.app.Application
+import com.kabigon.weatherforecast.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class WeatherApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // Log Koin into Android logger
+            androidLogger()
+            // Reference Android context
+            androidContext(this@WeatherApplication)
+            // Load modules
+            modules(appModule)
+        }
+    }
+}
+
+val appModule = listOf(
+    networkModule
+)
